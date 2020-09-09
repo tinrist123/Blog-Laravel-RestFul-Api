486642f059cd111583eddd0c72599ec18a0cbf01
Tin <tinrist123@gmail.com>
2020-09-09 20:39:03 +0700

    try again

--- database/seeds/PostsTableSeed.php
+++ database/seeds/PostsTableSeed.php
@@ -12,6 +12,6 @@ class PostsTableSeed extends Seeder
     public function run()
     {
         //
-        factory(App\Post::class, 50)->create();
+        factory(\App\Post::class, 50)->create();
     }
 }
