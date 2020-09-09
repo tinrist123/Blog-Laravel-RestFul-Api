53b0d776b9eb02e85b8246bd56fa8b89dd014e02
Tin <tinrist123@gmail.com>
2020-09-09 20:33:15 +0700

    final

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -11,10 +11,9 @@ $factory->define(\App\Post::class, function (Faker $faker) {
         //
         'user_id' => factory(App\User::class)->create(),
         'cate_id' => factory(App\Category::class)->create(),
-        'tag_id' => factory(App\Tag::class)->create(),
         'Title' => $faker->catchPhrase,
         'Alias' => $faker->domainWord,
-        'Shor_Description' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+        'Short_Description' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
         'Content' => $faker->text($maxNbChars = 5000),                        // 'Fuga totam reiciendis qui architecto fugiat nemo. Consequatur recusandae qui cupiditate eos quod.'
         'Image' => $faker->imageUrl($width = 1280, $height = 720),
         'created_at' => Carbon::now(),
